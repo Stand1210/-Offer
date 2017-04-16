5161f02948b202e190cebd2a2ff4c3c6ee2ee5a7
keqi@wb <shixi_keqi@staff.weibo.com>
2017-04-17 01:12:39 +0800

    Initial Commit

---
+++ offer/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  offer
+//
+//  Created by 宋珂琦 on 2017/4/17.
+//  Copyright © 2017年 stand. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
